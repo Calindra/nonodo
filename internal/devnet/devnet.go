57021831eaca9995b43fbae11001e1825a344edb
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-03-27 01:55:31 -0300

    style: fix via lint

--- internal/devnet/devnet.go
+++ internal/devnet/devnet.go
@@ -19,10 +19,10 @@ const SenderAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 const SenderPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 const (
-	BitmaskAddress             = "0xF5B2d8c81cDE4D6238bBf20D3D77DB37df13f735"
-	CartesiMathV2Address       = "0xB634F716BEd5Dd5A2b9a91C92474C499e50Cb27D"
-	MerkleV2Address            = "0x33436035441927Df1a73FE3AAC5906854632e53d"
-	UnrolledCordicAddress      = "0x3F8FdcD1B0F421D817BF58C96b7C91B98100B450"
+	BitmaskAddress        = "0xF5B2d8c81cDE4D6238bBf20D3D77DB37df13f735"
+	CartesiMathV2Address  = "0xB634F716BEd5Dd5A2b9a91C92474C499e50Cb27D"
+	MerkleV2Address       = "0x33436035441927Df1a73FE3AAC5906854632e53d"
+	UnrolledCordicAddress = "0x3F8FdcD1B0F421D817BF58C96b7C91B98100B450"
 	// Portal contracts
 
 	EtherPortalAddress         = "0xFfdbe43d4c855BF7e0f105c400A50857f53AB044"
@@ -31,13 +31,13 @@ const (
 	ERC1155SinglePortalAddress = "0x7CFB0193Ca87eB6e48056885E026552c3A941FC4"
 	ERC1155BatchPortalAddress  = "0xedB53860A6B52bbb7561Ad596416ee9965B055Aa"
 
-	DAppAddressRelayAddress    = "0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE"
-	CartesiDAppFactoryAddress  = "0x7122cd1221C20892234186facfE8615e6743Ab02"
-	AuthorityAddress           = "0x5050F233F2312B1636eb7CF6c7876D9cC6ac4785"
-	HistoryAddress             = "0x4FF8BD9122b7D91d56Dd5c88FE6891Fb3c0b5281"
-	SunodoTokenAddress         = "0xae7f61eCf06C65405560166b259C54031428A9C4"
-	AuthorityFactoryAddress    = "0x519421Bd7843e0D1E2F280490962850e31c86087"
-	PayableDAppSystemAddress   = "0x5314730B6285B9B026FAaD759aa39A9415eB874c"
+	DAppAddressRelayAddress   = "0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE"
+	CartesiDAppFactoryAddress = "0x7122cd1221C20892234186facfE8615e6743Ab02"
+	AuthorityAddress          = "0x5050F233F2312B1636eb7CF6c7876D9cC6ac4785"
+	HistoryAddress            = "0x4FF8BD9122b7D91d56Dd5c88FE6891Fb3c0b5281"
+	SunodoTokenAddress        = "0xae7f61eCf06C65405560166b259C54031428A9C4"
+	AuthorityFactoryAddress   = "0x519421Bd7843e0D1E2F280490962850e31c86087"
+	PayableDAppSystemAddress  = "0x5314730B6285B9B026FAaD759aa39A9415eB874c"
 )
 
 // Gas limit when sending transactions.
