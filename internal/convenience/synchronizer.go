8bf3c1ec76b7add9e5ff7e168c2e5ae1021c430e
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-04-07 01:31:43 -0300

    refactor: separate the voucher fetcher from the synchronizer.go

--- internal/convenience/synchronizer.go
+++ internal/convenience/synchronizer.go
@@ -1,12 +1,8 @@
 package convenience
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+	"log/slog"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -33,169 +29,45 @@ func (x Synchronizer) Start(ctx context.Context, ready chan<- struct{}) error {
 }
 
 func (x *Synchronizer) VoucherPolling(ctx context.Context) error {
-	// GraphQL endpoint URL
-	url := "http://localhost:8080/graphql"
-	after := ""
-	query := `query GetVouchers($after: String) {
-		vouchers(first: 1, after: $after) {
-			totalCount
-			edges{
-				node{
-					destination
-					payload
-					index
-					input {
-						index
-					}
-					proof {
-						validity {
-							inputIndexWithinEpoch
-							outputIndexWithinInput
-							outputHashesRootHash
-							vouchersEpochRootHash
-							noticesEpochRootHash
-							machineStateHash
-							outputHashInOutputHashesSiblings
-							outputHashesInEpochSiblings
-						}
-					context
-					}
+	voucherFetcher := NewVoucherFetcher()
+	sleepInSeconds := 3
+	for {
+		voucherResp, err := voucherFetcher.Fetch()
+		if err != nil {
+			slog.Warn(
+				"Voucher fetcher error, we will try again",
+				"error", err.Error(),
+			)
+		} else {
+			// Handle response data
+			for _, edge := range voucherResp.Data.Vouchers.Edges {
+				outputIndex := edge.Node.Index
+				inputIndex := edge.Node.Input.Index
+				slog.Debug("Add Voucher",
+					"inputIndex", inputIndex,
+					"outputIndex", outputIndex,
+				)
+				err := x.decoder.HandleOutput(ctx,
+					common.HexToAddress(edge.Node.Destination),
+					edge.Node.Payload,
+					uint64(inputIndex),
+					uint64(outputIndex),
+				)
+				if err != nil {
+					panic(err)
 				}
 			}
-			pageInfo {
-				startCursor
-				endCursor
-				hasNextPage
-				hasPreviousPage
+			if len(voucherResp.Data.Vouchers.PageInfo.EndCursor) > 0 {
+				voucherFetcher.CursorAfter = voucherResp.Data.
+					Vouchers.PageInfo.EndCursor
 			}
 		}
-		
-	}`
-	for {
-		fmt.Println("Querying...")
-		variables := map[string]interface{}{}
-		if len(after) > 0 {
-			variables = map[string]interface{}{
-				"after": after,
-			}
+		select {
+		// Wait a little before doing another request
+		case <-time.After(time.Duration(sleepInSeconds) * time.Second):
+		case <-ctx.Done():
+			slog.Debug("Synchronizer canceled:", ctx.Err())
+			return nil
 		}
-
-		payload, err := json.Marshal(map[string]interface{}{
-			"operationName": nil,
-			"query":         query,
-			"variables":     variables,
-		})
-		fmt.Printf("after %s\n", after)
-		if err != nil {
-			fmt.Println("Error marshalling JSON:", err)
-			return err
-		}
-
-		// Make a POST request to the GraphQL endpoint
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
-		if err != nil {
-			fmt.Println("Error creating request:", err)
-			continue
-		}
-
-		// Set request headers
-		req.Header.Set("Content-Type", "application/json")
-
-		// Send request
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("Error sending request:", err)
-			continue
-		}
-
-		// Read response body
-		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Println("Error reading response:", err)
-			continue
-		}
-
-		var response VoucherResponse
-		if err := json.Unmarshal(body, &response); err != nil {
-			fmt.Println("Error parsing JSON:", err)
-			continue
-		}
-
-		// Handle response data
-		for _, edge := range response.Data.Vouchers.Edges {
-			outputIndex := edge.Node.Index
-			inputIndex := edge.Node.Input.Index
-			fmt.Printf("InputIndex %d; OutputIndex %d;\n", inputIndex, outputIndex)
-			err := x.decoder.HandleOutput(ctx,
-				common.HexToAddress(edge.Node.Destination),
-				edge.Node.Payload,
-				uint64(inputIndex),
-				uint64(outputIndex),
-			)
-			if err != nil {
-				panic(err)
-			}
-		}
-		if len(response.Data.Vouchers.PageInfo.StartCursor) > 0 {
-			after = response.Data.Vouchers.PageInfo.StartCursor
-		}
-		// Wait for a certain duration before making the next request (e.g., 5 seconds)
-		sleepInSeconds := 3
-		time.Sleep(time.Duration(sleepInSeconds) * time.Second)
 	}
-}
-
-type VoucherResponse struct {
-	Data VoucherData `json:"data"`
-}
-
-type VoucherData struct {
-	Vouchers VoucherConnection `json:"vouchers"`
-}
-
-type VoucherConnection struct {
-	TotalCount int           `json:"totalCount"`
-	Edges      []VoucherEdge `json:"edges"`
-	PageInfo   PageInfo      `json:"pageInfo"`
-}
-
-type VoucherEdge struct {
-	Node   Voucher `json:"node"`
-	Cursor string  `json:"cursor"`
-}
-
-type Voucher struct {
-	Index       int      `json:"index"`
-	Destination string   `json:"destination"`
-	Payload     string   `json:"payload"`
-	Proof       Proof    `json:"proof"`
-	Input       InputRef `json:"input"`
-}
-
-type InputRef struct {
-	Index int `json:"index"`
-}
-type Proof struct {
-	Validity OutputValidityProof `json:"validity"`
-	Context  string              `json:"context"`
-}
-
-type OutputValidityProof struct {
-	InputIndexWithinEpoch            int      `json:"inputIndexWithinEpoch"`
-	OutputIndexWithinInput           int      `json:"outputIndexWithinInput"`
-	OutputHashesRootHash             string   `json:"outputHashesRootHash"`
-	VouchersEpochRootHash            string   `json:"vouchersEpochRootHash"`
-	NoticesEpochRootHash             string   `json:"noticesEpochRootHash"`
-	MachineStateHash                 string   `json:"machineStateHash"`
-	OutputHashInOutputHashesSiblings []string `json:"outputHashInOutputHashesSiblings"`
-	OutputHashesInEpochSiblings      []string `json:"outputHashesInEpochSiblings"`
-}
-
-type PageInfo struct {
-	StartCursor     string `json:"startCursor"`
-	EndCursor       string `json:"endCursor"`
-	HasNextPage     bool   `json:"hasNextPage"`
-	HasPreviousPage bool   `json:"hasPreviousPage"`
 }
