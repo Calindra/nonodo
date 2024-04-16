dc232077b8cf2852cfac4b199ffcf34f93abd58c
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-04-15 23:20:21 -0300

    feat: add GraphQL files and dependencies

---
+++ api/proxy-graphql/test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "log"
+	_ "net/http"
+
+	_ "github.com/99designs/gqlgen/handler"
+	_ "github.com/graphql-go/graphql"
+
+	_ "github.com/calindra/nonodo"
+)
+
+func test() {
+
+
+}
