3ac2c05e73aee88c2e3dcff15e018fbb67a5e887
Lyno Ferraz <lynoFerraz@gmail.com>
2024-05-08 19:07:09 -0300

    fix: Use the correct url and port for the ROLLUP_HTTP_SERVER_URL var pased to the dapp process

--- internal/nonodo/nonodo.go
+++ internal/nonodo/nonodo.go
@@ -219,12 +219,12 @@ func NewSupervisor(opts NonodoOpts) supervisor.SupervisorWorker {
 			Command: opts.ApplicationArgs[0],
 			Args:    opts.ApplicationArgs[1:],
 			Env: []string{fmt.Sprintf("ROLLUP_HTTP_SERVER_URL=http://%s:%v/rollup",
-				opts.HttpAddress, opts.HttpPort)},
+				opts.HttpAddress, opts.HttpRollupsPort)},
 		})
 	} else if opts.EnableEcho {
 		fmt.Println("Starting echo app")
 		w.Workers = append(w.Workers, echoapp.EchoAppWorker{
-			RollupEndpoint: fmt.Sprintf("http://127.0.0.1:%v", opts.HttpRollupsPort),
+			RollupEndpoint: fmt.Sprintf("http://%s:%v", opts.HttpAddress, opts.HttpRollupsPort),
 		})
 	}
 	return w
