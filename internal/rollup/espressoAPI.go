8f08d0481e672ef888b5510d1c5e11a599675d2d
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-05-08 21:06:26 -0300

    refactor: Filter transactions by VM ID in GetBlockByHeight method

--- internal/rollup/espressoAPI.go
+++ internal/rollup/espressoAPI.go
@@ -4,32 +4,47 @@ import (
 	"math/big"
 )
 
-type ExpressoHeader struct {
-	L1Finalized struct {
+type EspressoHeader struct {
+	Height *big.Int `json:"height"`
+
+	L1Finalized *struct {
 		Number *big.Int `json:"number"`
 	} `json:"l1_finalized"`
 }
 
+type EspressoTransactionNMT struct {
+	VM string `json:"vm"`
+}
+
+type EspressoPayload struct {
+	TransactionNMT []EspressoTransactionNMT `json:"transaction_nmt"`
+}
+
 type EspressoBlock struct {
-	Header ExpressoHeader `json:"header"`
+	Header  EspressoHeader  `json:"header"`
+	Payload EspressoPayload `json:"payload"`
+	Hash    string          `json:"hash"`
 }
 
-type BlockVM struct {
-	Payload struct {
-		TransactionNMT  []any `json:"transaction_nmt"`
-	} `json:"payload"`
-	Header struct {
-		Height *big.Int `json:"height"`
-	} `json:"header"`
-}
 
-func (b *EspressoBlock) filterByVM(vmId string) *BlockVM {
-	return nil
+func (b *EspressoBlock) filterByVM(vmId string) EspressoBlock {
+	filteredTransactions := []EspressoTransactionNMT{}
+	for _, transaction := range b.Payload.TransactionNMT {
+		if transaction.VM == vmId {
+			filteredTransactions = append(filteredTransactions, transaction)
+		}
+	}
+
+	return EspressoBlock{
+		Header:  b.Header,
+		Payload: EspressoPayload{TransactionNMT: filteredTransactions},
+		Hash:    b.Hash,
+	}
 }
 
 type EspressoAPI interface {
 	GetLatestBlockHeight() (*big.Int, error)
-	GetHeaderByBlockByHeight(height *big.Int) (*ExpressoHeader, error)
+	GetHeaderByBlockByHeight(height *big.Int) (*EspressoHeader, error)
 	GetBlockByHeight(height *big.Int) (*EspressoBlock, error)
 }
 
@@ -39,8 +54,8 @@ func (s *ExpressoService) GetLatestBlockHeight() (*big.Int, error) {
 	return big.NewInt(0), nil
 }
 
-func (s *ExpressoService) GetHeaderByBlockByHeight(height *big.Int) (*ExpressoHeader, error) {
-	return &ExpressoHeader{}, nil
+func (s *ExpressoService) GetHeaderByBlockByHeight(height *big.Int) (*EspressoHeader, error) {
+	return &EspressoHeader{}, nil
 }
 
 func (s *ExpressoService) GetBlockByHeight(height *big.Int) (*EspressoBlock, error) {
