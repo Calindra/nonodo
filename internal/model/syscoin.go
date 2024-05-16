46071c12431547f672f4e7198b78e7ac22cedb79
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-05-16 12:25:37 -0300

    feat: wip

--- internal/model/syscoin.go
+++ internal/model/syscoin.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/ethereum-optimism/optimism/op-service/client"
 	ethLog "github.com/ethereum/go-ethereum/log"
 	"github.com/labstack/echo/v4"
@@ -18,5 +21,15 @@ func Call() error {
 }
 
 func FetchSyscoinPoDa(ctx echo.Context, id string) (*string, *HttpCustomError) {
-	return nil, nil
+	// Read of file
+	file, err := os.ReadFile("syscoin-poda.json")
+
+	if err != nil {
+		return nil, NewHttpCustomError(http.StatusNotFound, nil)
+	}
+
+	// Convert to string
+	str := string(file)
+
+	return &str, nil
 }
