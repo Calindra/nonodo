b605f38cfa57472245c0a52530fe9830ef31f92a
Diego Guimaraes <diego.sena.guimaraes@gmail.com>
2024-05-20 11:51:45 -0300

    editing tests

--- test.js
+++ test.js
@@ -4,14 +4,26 @@ import { check } from 'k6';
 export let options = {
     stages: [
         { duration: '30s', target: 10 },
-        { duration: '30s', target: 10 },
         { duration: '10s', target: 0 },
     ],
 };
 
 export default function () {
-    let res = http.get('https://test.k6.io');
+    const url = 'http://localhost:8080/graphql'; 
+    const payload = JSON.stringify({
+        query: "query { voucher(voucherIndex: 0, inputIndex: 0) { index }}"
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
     check(res, {
         'is status 200': (r) => r.status === 200,
+        'response body contains expected content': (r) => r.body.includes('voucher not found'), 
     });
 }
