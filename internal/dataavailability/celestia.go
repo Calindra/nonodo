c4d90028a829ba568fa0885a25eb4f90554994bf
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-06-14 09:48:23 -0300

    chore: Submit and Get blob from Celestia in the same test is working

--- internal/dataavailability/celestia.go
+++ internal/dataavailability/celestia.go
@@ -5,54 +5,34 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
-	"fmt"
 	"log/slog"
+	"math/big"
+	"strings"
 
 	client "github.com/celestiaorg/celestia-openrpc"
 	"github.com/celestiaorg/celestia-openrpc/types/blob"
 	"github.com/celestiaorg/celestia-openrpc/types/share"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/tendermint/tendermint/rpc/client/http"
 )
 
-func GetProof() error {
-	ctx := context.Background()
-	trpc, err := http.New("tcp://localhost:26657", "/websocket")
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	err = trpc.Start()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	dcProof, err := trpc.DataRootInclusionProof(ctx, 15, 10, 20)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	fmt.Println(dcProof.Proof.String())
-	return nil
-}
-
 // SubmitBlob submits a blob containing "Hello, World!" to the 0xDEADBEEF namespace. It uses the default signer on the running node.
-func SubmitBlob(ctx context.Context, url string, token string) error {
+func SubmitBlob(ctx context.Context, url string, token string) (uint64, uint64, error) {
 	client, err := client.NewClient(ctx, url, token)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	// let's post to 0xDEADBEEF namespace
 	namespace, err := share.NewBlobNamespaceV0([]byte{0xDE, 0xAD, 0xBE, 0xEF})
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	// create a blob
 	helloWorldBlob, err := blob.NewBlobV0(namespace, []byte("Hello, World! Cartesi Rocks!"))
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	base64Str := base64.StdEncoding.EncodeToString(helloWorldBlob.Commitment)
@@ -66,12 +46,12 @@ func SubmitBlob(ctx context.Context, url string, token string) error {
 	height, err := client.Blob.Submit(ctx, []*blob.Blob{helloWorldBlob}, blob.DefaultGasPrice())
 	if err != nil {
 		slog.Error("Submit", "error", err)
-		return err
+		return 0, 0, err
 	}
 
 	bProof, err := client.Blob.GetProof(ctx, height, namespace, helloWorldBlob.Commitment)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	slog.Debug("Blob was included at",
@@ -84,7 +64,7 @@ func SubmitBlob(ctx context.Context, url string, token string) error {
 	// retrievedBlobs, err := client.Blob.GetAll(ctx, height, []share.Namespace{namespace})
 	retrievedBlob, err := client.Blob.Get(ctx, height, namespace, helloWorldBlob.Commitment)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	// slog.Debug("Blobs are equal?", "equal", bytes.Equal(helloWorldBlob.Commitment, retrievedBlobs[0].Commitment))
@@ -97,12 +77,12 @@ func SubmitBlob(ctx context.Context, url string, token string) error {
 
 	proof, err := client.Blob.GetProof(ctx, height, namespace, helloWorldBlob.Commitment)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	json, err := retrievedBlob.MarshalJSON()
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	slog.Debug("Proof",
@@ -110,5 +90,92 @@ func SubmitBlob(ctx context.Context, url string, token string) error {
 		"end", (*proof)[0].End(),
 		"index", string(json),
 	)
-	return nil
+	return height, uint64((*proof)[0].Start()), nil
+}
+
+func getABI() (*abi.ABI, error) {
+	jsonABI := `[
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "namespace",
+					"type": "bytes32"
+				},
+				{
+					"name": "height",
+					"type": "uint256"
+				},
+				{
+					"name": "start",
+					"type": "uint256"
+				}
+			],
+			"name": "CelestiaRequest",
+			"outputs": [
+				{
+					"name": "",
+					"type": "bytes"
+				}
+			],
+			"payable": false,
+			"stateMutability": "pure",
+			"type": "function"
+		}
+	]`
+	parsed, err := abi.JSON(strings.NewReader(jsonABI))
+	if err != nil {
+		return nil, err
+	}
+	return &parsed, nil
+}
+
+func parseParams(id string) (*[32]uint8, *big.Int, *big.Int, error) {
+	abiParsed, err := getABI()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	values, err := abiParsed.Methods["CelestiaRequest"].Inputs.UnpackValues(common.Hex2Bytes(id[10:]))
+	if err != nil {
+		slog.Error("Error unpacking blob.", "err", err)
+		return nil, nil, nil, err
+	}
+	namespace := values[0].([32]uint8)
+	height := values[1].(*big.Int)
+	start := values[2].(*big.Int)
+	return &namespace, height, start, nil
+}
+
+func GetBlob(ctx context.Context, id string, url string, token string) ([]byte, error) {
+	namespace, height, start, err := parseParams(id)
+	if err != nil {
+		return nil, err
+	}
+	namespaceV0, err := share.NewBlobNamespaceV0(namespace[22:])
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("CelestiaRequest",
+		"namespaceV0", common.Bytes2Hex(namespaceV0[:]),
+		"height", height,
+		"start", start,
+	)
+	client, err := client.NewClient(ctx, url, token)
+	if err != nil {
+		return nil, err
+	}
+	// namespaceV0, err = share.NewBlobNamespaceV0([]byte{0xDE, 0xAD, 0xBE, 0xEF})
+
+	celestiaHeight := height.Uint64()
+	celestiaNamespace := []share.Namespace{namespaceV0}
+	retrievedBlobs, err := client.Blob.GetAll(ctx, celestiaHeight, celestiaNamespace)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("Blobs",
+		"len", len(retrievedBlobs),
+		"data", string(retrievedBlobs[0].Blob.Data),
+	)
+
+	return retrievedBlobs[0].Blob.Data, nil
 }
--- internal/dataavailability/celestia_test.go
+++ internal/dataavailability/celestia_test.go
@@ -3,10 +3,12 @@ package dataavailability
 import (
 	"context"
 	"log/slog"
+	"math/big"
 	"os"
 	"testing"
 
 	"github.com/calindra/nonodo/internal/commons"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -19,25 +21,95 @@ func (s *CelestiaSuite) SetupTest() {
 }
 
 func (s *CelestiaSuite) TestSubmitBlob() {
-	token := os.Getenv("CELESTIA_AUTH_TOKEN")
-	// url := os.Getenv("CELESTIA_URL")
+	token := os.Getenv("TIA_AUTH_TOKEN")
+	url := os.Getenv("TIA_URL")
 	// url := "https://api.celestia-arabica-11.com" //os.Getenv("CELESTIA_URL")
 	// url := "https://validator-3.celestia-arabica-11.com:26657"
 	// url := "https://26658-calindra-celestianode-p9zxr391sw1.ws-us114.gitpod.io"
-	url := "https://26658-calindra-celestianode-p9zxr391sw1.ws-us114.gitpod.io"
+	// url := "https://26658-calindra-celestianode-p9zxr391sw1.ws-us114.gitpod.io"
 	// url := "https://rpc.celestia-mocha.com:26658" // not working
 	if token == "" || url == "" {
 		slog.Debug("missing celestia configuration")
 		return
 	}
+	slog.Debug("Configs", "url", url, "token", token)
 	ctx := context.Background()
-	err := SubmitBlob(ctx, url, token)
+	heightU, startU, err := SubmitBlob(ctx, url, token)
 	s.NoError(err)
+
+	// test the fetch
+	abiParsed, err := getABI()
+	s.NoError(err)
+	// dead beef
+	stringNamespace := "00000000000000000000000000000000000000000000000000000000deadbeef"
+	namespace := common.Hex2Bytes(stringNamespace)
+	var bytes32Value [32]byte
+	copy(bytes32Value[:], namespace)
+	height := big.NewInt(int64(heightU))
+	start := big.NewInt(int64(startU))
+	payload, err := abiParsed.Pack(
+		"CelestiaRequest",
+		bytes32Value,
+		&height,
+		&start,
+	)
+	s.NoError(err)
+	id := "0x" + common.Bytes2Hex(payload)
+	slog.Debug("ID",
+		"id", id,
+		"namespace", namespace,
+	)
+	data, err := GetBlob(ctx, id, url, token)
+	s.NoError(err)
+	slog.Debug("GetBlob",
+		"data", string(data),
+	)
 	s.Fail("123")
 }
 
-func (s *CelestiaSuite) TestSubmitProof() {
+func (s *CelestiaSuite) XTestGioRequest() {
+	abiParsed, err := getABI()
+	s.NoError(err)
+	// dead beef
+	stringNamespace := "00000000000000000000000000000000000000000000000000000000deadbeef"
+	namespace := common.Hex2Bytes(stringNamespace)
+	var bytes32Value [32]byte
+	copy(bytes32Value[:], namespace)
+	height := big.NewInt(1490181)
+	start := big.NewInt(1)
+	// height := big.NewInt(2040311)
+	// start := big.NewInt(14)
 
+	// height := big.NewInt(731137)
+	// start := big.NewInt(0)
+	payload, err := abiParsed.Pack(
+		"CelestiaRequest",
+		bytes32Value,
+		&height,
+		&start,
+	)
+	s.NoError(err)
+	id := "0x" + common.Bytes2Hex(payload)
+	slog.Debug("ID",
+		"id", id,
+		"namespace", namespace,
+	)
+	ctx := context.Background()
+	token := os.Getenv("TIA_AUTH_TOKEN")
+	url := os.Getenv("TIA_URL")
+	// url := "https://api.celestia-arabica-11.com" //os.Getenv("CELESTIA_URL")
+	// url := "https://validator-3.celestia-arabica-11.com:26657"
+	// url := "https://26658-calindra-celestianode-p9zxr391sw1.ws-us114.gitpod.io"
+	// url := "https://26658-calindra-celestianode-p9zxr391sw1.ws-us114.gitpod.io"
+	// url := "https://rpc.celestia-mocha.com:26658" // not working
+	if token == "" || url == "" {
+		slog.Debug("missing celestia configuration")
+		return
+	}
+	data, err := GetBlob(ctx, id, url, token)
+	s.NoError(err)
+	slog.Debug("GetBlob", "data", string(data))
+	// s.Fail("x")
 }
 
 func TestCelestiaSuite(t *testing.T) {
