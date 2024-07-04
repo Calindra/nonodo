0fe0527557dd2e668d2f7dc16ed293f48fe856c8
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-07-04 02:46:46 -0300

    feat: Update listTags function to accept logger parameter
    The listTags function in version-manager.js now accepts a logger parameter, allowing for more flexibility in logging. This change enables the caller to pass in a custom logger instance, which can be useful for different logging configurations or environments.

--- npm-package/src/version-manager.js
+++ npm-package/src/version-manager.js
@@ -4,27 +4,40 @@
 import { Logger, Levels } from "./logger.js"
 import { makeRequest } from "./utils.js";
 
-const logger = new Logger("Brunodo", Levels.DEBUG);
+const logger = new Logger("Brunodo", Levels.INFO);
 
-async function listTags(signal) {
+async function listTags(signal, logger) {
     const repo = "nonodo"
     const namespace = "calindra"
     const url = new URL(`https://api.github.com/repos/${namespace}/${repo}/tags`)
-    logger.info(url)
+    logger.info(`Requesting tags from ${url}`)
     const res = await makeRequest(signal, url)
     const tags = JSON.parse(res.toString())
     logger.debug(tags)
+    if (!Array.isArray(tags)) {
+        throw new Error("Invalid response")
+    }
     const names = tags.map((tag) => tag.name)
-    logger.info(names)
+    logger.info(`Valid tags: ${names.join(", ")}`)
     return names
 }
 
 
 async function main() {
     const abortCtrl = new AbortController()
-    await listTags(abortCtrl.signal)
+    const args = process.argv.slice(2);
+    const isDebug = args.includes("--debug")
+    const level = isDebug ? Levels.DEBUG : Levels.INFO
+    const logger = new Logger("Brunodo", level);
 
-    return false
+    switch (args[0]) {
+        case "list":
+            await listTags(abortCtrl.signal, logger)
+            return true
+        default:
+            logger.error(`Unknown command: ${args[0]}`)
+            return false
+    }
 }
 
 main().then((success) => {
