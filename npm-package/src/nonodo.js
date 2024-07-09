a3404d9a890923e63fdca1a5fd0966ef094a4a6c
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-07-09 18:11:04 -0300

    refactor: Update import statements and add folder check hook

--- npm-package/src/nonodo.js
+++ npm-package/src/nonodo.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 
-import { flush, handle, execute, run } from "@oclif/core";
+import { flush, handle } from "@oclif/core";
 import { Run } from "./commands/run.js";
-import { join } from "node:path"
-import { pathToFileURL } from "node:url"
 
 async function main() {
-  const path = join(import.meta.dirname, "../nonodo.oclifrc.json");
-  const pathURL = pathToFileURL(path)
-
   try {
-    await Run.run(undefined, pathURL.href)
+    await Run.run([], import.meta.dirname)
     await flush()
   } catch (err) {
     console.trace(err)
