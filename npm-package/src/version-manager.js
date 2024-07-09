52234b87e47eb6d7e5186b1ca8020862afc0ec4a
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-07-09 05:58:46 -0300

    Update npm package dependencies and fix import paths

--- npm-package/src/version-manager.js
+++ npm-package/src/version-manager.js
@@ -7,34 +7,3 @@ import { execute } from "@oclif/core";
 execute({
   dir: import.meta.url,
 });
-
-
-// async function main() {
-//   await execute({
-//     dir: import.meta.url,
-//     // development: process.env.NODE_ENV === "development",
-//     // loadOptions: {
-//     //   root: import.meta.dirname,
-//     // }
-//   });
-
-//   return true;
-// }
-
-// main()
-//   .then((success) => {
-//     if (!success) {
-//       process.exit(1);
-//     }
-//   })
-//   .catch((e) => {
-//     const logger = new Logger("Brunodo", Levels.ERROR)
-
-//     if (e instanceof Error) {
-//       logger.error(e.stack);
-//     } else {
-//       logger.error(e);
-//     }
-
-//     process.exit(1);
-//   });
