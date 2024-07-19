6a91227aabbc669af18ef20ead18dcaa450baddd
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-07-19 15:28:24 -0300

    fix(k6): fix ci from k6

--- loadtesting/test.js
+++ loadtesting/test.js
@@ -13,6 +13,10 @@ export let options = {
 
 const GRAPHQL_ENDPOINT = 'http://localhost:8080/graphql'
 
+function assertStringContains(s, substring) {
+    return typeof s === "string" && typeof substring === "string" && s.includes(substring);
+}
+
 function testVoucherNotFound() {
     const payload = JSON.stringify({
         query: "query { voucher(voucherIndex: 0, inputIndex: 0) { index }}"
@@ -28,7 +32,7 @@ function testVoucherNotFound() {
 
     check(response, {
         'testVoucherNotFound is status 200': (r) => r.status === 200,
-        'testVoucherNotFound response body contains expected content': (r) => r.body.includes('voucher not found'), 
+        'testVoucherNotFound response body contains expected content': (r) => assertStringContains(r.body, 'voucher not found'),
     });
 }
 
@@ -47,7 +51,7 @@ function testVoucherFound() {
 
     check(response, {
         'testVoucherFound is status 200': (r) => r.status === 200,
-        'testVoucherFound response body contains expected content': (r) => r.body.includes('{"data":{"voucher":{"index":1}}}'), 
+        'testVoucherFound response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"voucher":{"index":1}}}'),
     });
 }
 
@@ -67,7 +71,7 @@ function testNoticeFound() {
 
     check(response, {
         'testNoticeFound is status 200': (r) => r.status === 200,
-        'testNoticeFound response body contains expected content': (r) => r.body.includes('{"data":{"notice":{"index":1,"payload":"OX1223"}}}'), 
+        'testNoticeFound response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"notice":{"index":1,"payload":"OX1223"}}}'),
     });
 }
 
@@ -87,14 +91,14 @@ function testInputFound() {
 
     check(response, {
         'testInputFound is status 200': (r) => r.status === 200,
-        'testInputFound response body contains expected content': (r) => r.body.includes('{"data":{"input":{"index":1}}}'), 
+        'testInputFound response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"input":{"index":1}}}'),
     });
 }
 
 function testReportFound() {
     const payload = JSON.stringify({
         query: "query { report(reportIndex: 2, inputIndex: 2) { index }}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -106,14 +110,14 @@ function testReportFound() {
 
     check(response, {
         'testReportFound is status 200': (r) => r.status === 200,
-        'testReportFound response body contains expected content': (r) => r.body.includes('{"data":{"report":{"index":2}}}'), 
+        'testReportFound response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"report":{"index":2}}}'),
     });
 }
 
 function testConvenientVouchers() {
     const payload = JSON.stringify({
         query: "query { convenientVouchers(first: 10) { edges { node { index }}}}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -125,14 +129,14 @@ function testConvenientVouchers() {
 
     check(response, {
         'testConvenientVouchers is status 200': (r) => r.status === 200,
-        'testConvenientVouchers response body contains expected content': (r) => r.body.includes('{"data":{"convenientVouchers":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'), 
+        'testConvenientVouchers response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"convenientVouchers":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'),
     });
 }
 
 function testReports() {
     const payload = JSON.stringify({
         query: "query { reports(first: 10) { edges { node { index }}}}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -144,14 +148,14 @@ function testReports() {
 
     check(response, {
         'testReports is status 200': (r) => r.status === 200,
-        'testReports response body contains expected content': (r) => r.body.includes('{"data":{"reports":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'), 
+        'testReports response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"reports":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'),
     });
 }
 
 function testVouchers() {
     const payload = JSON.stringify({
         query: "query { vouchers(first: 10) { edges { node { index }}}}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -163,14 +167,14 @@ function testVouchers() {
 
     check(response, {
         'testVouchers is status 200': (r) => r.status === 200,
-        'testVouchers response body contains expected content': (r) => r.body.includes('{"data":{"vouchers":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'), 
+        'testVouchers response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"vouchers":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'),
     });
 }
 
 function testNotices() {
     const payload = JSON.stringify({
         query: "query { notices(first: 10) { edges { node { index }}}}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -182,14 +186,14 @@ function testNotices() {
 
     check(response, {
         'testNotices is status 200': (r) => r.status === 200,
-        'testNotices response body contains expected content': (r) => r.body.includes('{"data":{"notices":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'), 
+        'testNotices response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"notices":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'),
     });
 }
 
 function testInputs() {
     const payload = JSON.stringify({
         query: "query { inputs(first: 10) { edges { node { index }}}}"
-    }); 
+    });
 
     const params = {
         headers: {
@@ -201,19 +205,19 @@ function testInputs() {
 
     check(response, {
         'testInputs is status 200': (r) => r.status === 200,
-        'testInputs response body contains expected content': (r) => r.body.includes('{"data":{"inputs":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'), 
+        'testInputs response body contains expected content': (r) => assertStringContains(r.body, '{"data":{"inputs":{"edges":[{"node":{"index":1}},{"node":{"index":2}}]}}}'),
     });
 }
 
 export default function () {
-   testVoucherNotFound()
-   testVoucherFound()
-   testNoticeFound()
-   testInputFound()
-   testReportFound()
-   testConvenientVouchers()
-   testVouchers()
-   testNotices()
-   testReports()
-   testInputs()
+    testVoucherNotFound()
+    testVoucherFound()
+    testNoticeFound()
+    testInputFound()
+    testReportFound()
+    testConvenientVouchers()
+    testVouchers()
+    testNotices()
+    testReports()
+    testInputs()
 }
