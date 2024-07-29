1d30e46ff1dc1dd528afd71f08e2111513458fe7
Vinicius Catarino <vinicius.catarino@calindra.com.br>
2024-07-29 15:26:39 -0300

    test: testing transaction rollback

--- internal/convenience/synchronizer/graphile_synchornizer_test.go
+++ internal/convenience/synchronizer/graphile_synchornizer_test.go
@@ -260,6 +260,7 @@ func TestCommit_handleWithDBTransaction(t *testing.T) {
 	}
 
 	var count int
+	expectedRows := 0
 	err = synchronizer.SynchronizerRepository.GetDB().Get(&count, "SELECT COUNT(*) FROM synchronizer_fetch")
 	if err != nil {
 		t.Fatalf("Error checking the number of rows in the 'synchronizer_fetch' table: %v", err)
@@ -279,16 +280,17 @@ func TestCommit_handleWithDBTransaction(t *testing.T) {
 	}
 
 	var otherCount int
+	expectedRows = 1
 	err = synchronizer.SynchronizerRepository.GetDB().Get(&otherCount, "SELECT COUNT(*) FROM synchronizer_fetch")
 	if err != nil {
 		t.Fatalf("Error checking the number of rows in the 'synchronizer_fetch' table: %v", err)
 	}
 
-	require.Equal(t, 1, otherCount, "The table should have one row.")
+	require.Equal(t, expectedRows, otherCount, "The table should have one row.")
 
 }
 
-func TestContextWithTimeout_Failure(t *testing.T) {
+func TestRollback_handleWithDBTransaction(t *testing.T) {
 	db := sqlx.MustConnect("sqlite3", ":memory:")
 	defer db.Close()
 
@@ -306,139 +308,25 @@ func TestContextWithTimeout_Failure(t *testing.T) {
 		panic(err)
 	}
 
-	// Verificar se a tabela foi criada
-	var count int
-	err = db.Get(&count, "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='synchronizer_fetch';")
-	if err != nil {
-		t.Fatalf("Erro ao verificar se a tabela foi criada: %v", err)
-	}
-
-	if count == 0 {
-		t.Fatalf("A tabela synchronizer_fetch não foi criada.")
-	}
-
-	fmt.Println("A tabela synchronizer_fetch foi criada com sucesso.")
-
-	dataBase := synchronizer.SynchronizerRepository.GetDB()
-	ctx, err := repository.StartTransaction(context.Background(), dataBase)
-	require.NoError(t, err)
-
-	if err != nil {
-		fmt.Printf("Fail to initialize transaction on test %v \n", err)
-	}
-
-	tx, err := repository.GetTransaction(ctx)
-
-	if err != nil {
-		fmt.Printf("Recovery transaction fail on test %v \n", err)
-	}
-	defer tx.Rollback()
-
-	syncFetch := &model.SynchronizerFetch{
-		Id:                   1,
-		TimestampAfter:       1234567890,
-		IniCursorAfter:       "cursor_1",
-		LogVouchersIds:       "voucher_123,voucher_456",
-		EndCursorAfter:       "cursor_2",
-		IniInputCursorAfter:  "input_cursor_1",
-		EndInputCursorAfter:  "input_cursor_2",
-		IniReportCursorAfter: "report_cursor_1",
-		EndReportCursorAfter: "report_cursor_2",
-	}
-
-	_, err = synchronizer.SynchronizerRepository.Create(ctx, syncFetch)
-
-	if err != nil {
-		fmt.Printf("Erro ao criar SynchronizerFetch: %v\n", err)
-	}
-
-	var fetched model.SynchronizerFetch
-	err = tx.GetContext(context.Background(), &fetched, "SELECT * FROM synchronizer_fetch WHERE id = ?", syncFetch.Id)
-	require.NoError(t, err, "Erro ao recuperar dados")
-
-	require.Equal(t, syncFetch.Id, fetched.Id, "ID não corresponde")
-	require.Equal(t, syncFetch.TimestampAfter, fetched.TimestampAfter, "TimestampAfter não corresponde")
-	require.Equal(t, syncFetch.IniCursorAfter, fetched.IniCursorAfter, "IniCursorAfter não corresponde")
-	require.Equal(t, syncFetch.LogVouchersIds, fetched.LogVouchersIds, "LogVouchersIds não corresponde")
-	require.Equal(t, syncFetch.EndCursorAfter, fetched.EndCursorAfter, "EndCursorAfter não corresponde")
-	require.Equal(t, syncFetch.IniInputCursorAfter, fetched.IniInputCursorAfter, "IniInputCursorAfter não corresponde")
-	require.Equal(t, syncFetch.EndInputCursorAfter, fetched.EndInputCursorAfter, "EndInputCursorAfter não corresponde")
-	require.Equal(t, syncFetch.IniReportCursorAfter, fetched.IniReportCursorAfter, "IniReportCursorAfter não corresponde")
-	require.Equal(t, syncFetch.EndReportCursorAfter, fetched.EndReportCursorAfter, "EndReportCursorAfter não corresponde")
-
-	erro := errors.New("Handle Output Value")
+	err = errors.New("Handle Output Value")
 
 	decoderMock.On("RetrieveDestination", mock.Anything).Return(common.Address{}, nil)
 	decoderMock.On("HandleOutputV2", mock.Anything, mock.Anything).Return(nil)
 	decoderMock.On("HandleInput", mock.Anything, mock.Anything, mock.Anything).Return(nil)
-	decoderMock.On("HandleReport", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(erro)
+	decoderMock.On("HandleReport", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(err)
 	outputResponse := getTestOutputResponse()
 
 	err = synchronizer.handleWithDBTransaction(outputResponse)
 	if err != nil {
 		fmt.Println("ERRO handleWithDBTransaction ")
-		// tx.Rollback()
 	}
 
-	var otherCount int
-	err = tx.GetContext(context.Background(), &otherCount, "SELECT COUNT(*) FROM synchronizer_fetch")
+	var count int
+	expectedRows := 0
+	err = synchronizer.SynchronizerRepository.GetDB().Get(&count, "SELECT COUNT(*) FROM synchronizer_fetch")
 	if err != nil {
-		t.Fatalf("Erro ao verificar se a tabela foi criada: %v", err)
+		t.Fatalf("Error checking the number of rows in the 'synchronizer_fetch' table: %v", err)
 	}
 
-	var otherFetched model.SynchronizerFetch
-	err = tx.GetContext(context.Background(), &otherFetched, "SELECT * FROM synchronizer_fetch WHERE id = ?", syncFetch.Id)
-	require.NoError(t, err, "Erro ao recuperar dados")
-
-	// Exibir o resultado na tela
-	// fmt.Printf("Dados recuperados:\n")
-	// fmt.Printf("ID: %d\n", otherFetched.Id)
-	// fmt.Printf("Timestamp After: %d\n", otherFetched.TimestampAfter)
-	// fmt.Printf("Ini Cursor After: %s\n", otherFetched.IniCursorAfter)
-	// fmt.Printf("Log Vouchers IDs: %s\n", otherFetched.LogVouchersIds)
-	// fmt.Printf("End Cursor After: %s\n", otherFetched.EndCursorAfter)
-	// fmt.Printf("Ini Input Cursor After: %s\n", otherFetched.IniInputCursorAfter)
-	// fmt.Printf("End Input Cursor After: %s\n", otherFetched.EndInputCursorAfter)
-	// fmt.Printf("Ini Report Cursor After: %s\n", otherFetched.IniReportCursorAfter)
-	// fmt.Printf("End Report Cursor After: %s\n", otherFetched.EndReportCursorAfter)
-
-	// Confirmar a transação
-	// err = tx.Commit()
-	// require.NoError(t, err, "Erro ao confirmar a transação")
-
-	require.Equal(t, 0, otherCount, "A Tabela deveria estar vazia")
-
+	require.Equal(t, expectedRows, count, "The table should be empty.")
 }
-
-// func TestContextWithTimeout_Failure(t *testing.T) {
-// 	db, sqlmock, err := sqlmock.New()
-// 	assert.NoError(t, err)
-// 	defer db.Close()
-
-// 	sqlmock.ExpectBegin()
-
-// 	sqlxDB := sqlx.NewDb(db, "sqlmock")
-// 	tx, err := sqlxDB.BeginTxx(context.Background(), nil)
-// 	assert.NoError(t, err)
-
-// 	syncRepoMock := &MockSynchronizerRepository{}
-
-// 	response := getTestOutputResponse()
-
-// 	decoderMock := &DecoderInterfaceMock{}
-
-// 	synchronizer := GraphileSynchronizer{
-// 		Decoder:                decoderMock,
-// 		SynchronizerRepository: syncRepoMock,
-// 		GraphileFetcher:        &GraphileFetcher{},
-// 	}
-
-// 	erro := errors.New("Handle Output Value")
-
-// 	syncRepoMock.On("BeginTxx", mock.Anything).Return(tx, nil)
-// 	decoderMock.On("RetrieveDestination", mock.Anything).Return(common.Address{}, nil)
-// 	decoderMock.On("HandleOutputV2", mock.Anything, mock.Anything).Return(erro)
-
-// 	synchronizer.handleWithDBTransaction(response)
-
-// }
